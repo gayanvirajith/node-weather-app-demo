b503d8d015fb392507d05055c676b7da39e4d459
Gayan Virajith <gayanvirajith@gmail.com>
2017-07-06 23:49:42 +0530

    Integrate yargs

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv.a);
+
+var encodedAddress = encodeURIComponent(argv.address);
+// var encodedAddress = '1301%20lombard%20street%20philadelphia';
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia'
+  url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
 }, (error, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2));
   // console.log(body);
